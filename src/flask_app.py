381f90b0a71483fb3cf7d7c9f8993ce0f324f602
Expo2024 <cloudexpo2024@gmail.com>
2024-10-06 17:56:49 -0300

    add requirements.txt file

--- src/flask_app.py
+++ src/flask_app.py
@@ -2,17 +2,20 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from cloudinary import api
 import cloudinary
+
+cloudinary.config(
+    cloud_name = "dsf5jskjk",
+    api_key = "172248515715777",
+    api_secret = "RiQZVf2iukLHdmdMnk8za5jWpkE",
+    api_proxy = "https://proxy.server:3128"  # Asegúrate de usar el proxy correcto
+)
+
+
 import cloudinary.uploader
-from decouple import config
+import cloudinary.api
 
 app = Flask(__name__)
 CORS(app)
-
-cloudinary.config(
-    cloud_name = config("CLOUD_NAME"),
-    api_key = config("CLOUD_API_KEY"),
-    api_secret = config("CLOUD_API_SECRET")
-)
 
 @app.route('/')
 def index():
@@ -22,14 +25,14 @@ def index():
 def upload():
     if 'file' not in request.files:
         return jsonify({'error': 'nofile'}), 400
-    
+
     file = request.files['file']
 
     try:
         response = cloudinary.uploader.upload(file,folder='uploads')
 
         return jsonify({'message': 'file uploaded','url': response['secure_url']}), 200
-    
+
     except Exception as error:
         return jsonify({'error': error}), 500
 
@@ -39,8 +42,9 @@ def get_images():
         response = api.resources(type='upload', prefix='uploads/')
 
         return jsonify({'message': 'images retrieved', "images": response['resources']}), 200
-    except Exception as error:
-        return jsonify({'error': error}), 500
+    except cloudinary.exceptions.GeneralError as e:
+        return jsonify({'error': 'Cloudinary error occurred: ' + str(e)}), 500
+    except Exception as e:
+        return jsonify({'error': 'An unexpected error occurred: ' + str(e)}), 500
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug=False)
